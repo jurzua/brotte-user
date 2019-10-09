8f7c1460334b9acf71ce3ae98d7ba5693a93f692
Ricardo <rurzua990@hotmail.com>
2019-10-09 11:27:06 -0400

    my first commit

--- src/main/java/cl/brotte/user/UserApplication.java
+++ src/main/java/cl/brotte/user/UserApplication.java
@@ -3,6 +3,7 @@ package cl.brotte.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*My first push*/
 @SpringBootApplication
 public class UserApplication {
 
